d9bd5b13d3c43c34ec3cefd0b4be25f25efc83f3
David Johnston <david@blacksheepcode.com>
2023-06-21 13:09:43 +1000

    Initial commit

---
+++ jest.config.ts
@@ -0,0 +1,17 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  transform: {
+    "^.+\\.jsx?$": "babel-jest",
+    "^.+\\.svg$": "jest-transformer-svg"
+  },
+  preset: "ts-jest",
+  testEnvironment: "node",
+  globals: {
+    "ts-jest": {
+      isolatedModules: true
+    }
+  }
+};
+
+export default config;
